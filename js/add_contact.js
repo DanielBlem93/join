1c7fc9955d8cbd044a7cda64e04a93cbcdce8944
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-07-20 14:22:06 +0200

    add contact modal

---
+++ js/add_contact.js
@@ -0,0 +1,6 @@
+function openModalAddContakt() {
+    document.getElementById('add-contakt-modal').style.right = '0';
+}
+function closeModal(){
+    document.getElementById('add-contakt-modal').style.right = '-200%';
+}
