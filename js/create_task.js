648237565d277f1a3513e87be9a24d76d80fafaf
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-07-31 09:55:43 +0200

    fixed bug

--- js/create_task.js
+++ js/create_task.js
@@ -11,20 +11,12 @@ let task = [];
  */
 async function createTaskBackend(newTask) {
     let taskData = await getItem('task');
-
-    // Check if there's data already
     if (!taskData) {
-        // If not, initialize the task array
         task = [];
     } else {
-        // If yes, parse the existing data
         task = JSON.parse(taskData);
     }
-
-    // Add the new task
     task.push(newTask);
-
-    // Save back to storage
     await setItem('task', JSON.stringify(task));
 
     console.log('task', task);
